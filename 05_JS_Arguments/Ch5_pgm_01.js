88a2690606d1d11d385d1c50046df0962eda6797
Archanakavitha <137041817+Archanakavitha@users.noreply.github.com>
2024-07-15 21:05:45 +0530

    5

--- 05_JS_Arguments/Ch5_pgm_01.js
+++ 05_JS_Arguments/Ch5_pgm_01.js
@@ -3,14 +3,15 @@
 var message;
 var showMessage;
 
-message = "It's full of stars!";
+
+message = "The moon is looking beautiful!";
 
 showMessage = function () {
-	console.log(message);
+	
+    console.log(msg);
 };
 
 showMessage();
-
 
 
 /* Further Adventures
