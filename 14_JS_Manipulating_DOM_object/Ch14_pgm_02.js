798e03844542155a59970398ac97ba79ce4c2de6
Archanakavitha <137041817+Archanakavitha@users.noreply.github.com>
2024-07-16 16:48:09 +0530

    14

--- 14_JS_Manipulating_DOM_object/Ch14_pgm_02.js
+++ 14_JS_Manipulating_DOM_object/Ch14_pgm_02.js
@@ -1,5 +1,14 @@
 // Use the countries array to display all the countries. 
+const countries = ['Afghanistan','Bangladesh', 'Barbados', 'Belarus', 'Belgium','Sri Lanka', 'Sudan', 'Suriname', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', 'India'];
 
+const countriesList = document.getElementById('countries-list');
+
+// Loop through the countries array and create list items for each country
+countries.forEach(country => {
+    const li = document.createElement('li');
+    li.textContent = country;
+    countriesList.appendChild(li);
+});
 
 //See the design
 
