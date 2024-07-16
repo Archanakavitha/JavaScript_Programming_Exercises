1b37433755ab6aedd8b1ff9ce43b29f7794ae0df
Archanakavitha <137041817+Archanakavitha@users.noreply.github.com>
2024-07-16 17:05:53 +0530

    21

--- 21_JS_Regular_expressions/Ch21_pgm_01.js
+++ 21_JS_Regular_expressions/Ch21_pgm_01.js
@@ -1,5 +1,17 @@
 // Write a pattern which identify if a string is a valid JavaScript variable
+function is_valid_variable(variableName) {
+    const validVariablePattern = /^[a-zA-Z_$][a-zA-Z0-9_$]*$/;
 
+    // Test if the variableName matches the pattern
+    return validVariablePattern.test(variableName);
+}
+
+
+
+console.log(is_valid_variable('first_name')); // true
+console.log(is_valid_variable('first-name')); // false
+console.log(is_valid_variable('1first_name')); // false
+console.log(is_valid_variable('firstname')); // true
 // is_valid_variable('first_name') # True
 // is_valid_variable('first-name') # False
 // is_valid_variable('1first_name') # False
